ee6c702db523b5ad15f2af0ada639041b4a24e89
pablocom <p.c.ramirez5150@gmail.com>
2021-10-18 13:37:33 +0200

    Moves OrSpecification to another file

--- DesignPatterns.Specification/Flight.cs
+++ DesignPatterns.Specification/Flight.cs
@@ -37,22 +37,4 @@ namespace DesignPatterns.Specification
             }
         }
     }
-
-    public class OrSpecification<T> : ISpecification<T>
-    {
-        private readonly ISpecification<T> _firstSpecification;
-        private readonly ISpecification<T> _secondSpecification;
-
-        public OrSpecification(ISpecification<T> firstSpecification, ISpecification<T> secondSpecification)
-        {
-            _firstSpecification = firstSpecification;
-            _secondSpecification = secondSpecification;
-        }
-
-        public bool IsSatisfiedBy(T entity)
-        {
-            return _firstSpecification.IsSatisfiedBy(entity) || _secondSpecification.IsSatisfiedBy(entity);
-        }
-    
-    }
 }
